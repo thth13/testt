5cf2b0befbe35c5620116ab060d57cf629bfd392
Dmitriy Khutornoy <thth13q@gmail.com>
2024-08-31 23:25:17 +0300

    fix

--- index.ts
+++ index.ts
@@ -6,6 +6,6 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(8080, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
